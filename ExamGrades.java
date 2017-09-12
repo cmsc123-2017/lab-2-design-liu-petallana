652bfba10e2cbed9b86dd8d0eb8069bda7e259b9
Jeane Petallana <mhpetallana@up.edu.ph>
2017-09-12 21:21:37 +0800

    Modified

--- ExamGrades.java
+++ ExamGrades.java
@@ -7,4 +7,22 @@ class ExamGrades {
   
   // TODO design a function that returns the highest grade
   
+  /* -> double
+   * Returns the highest grade given an array of grades.
+   */
+  
+  /* Template
+   *   Fields:
+   *     ... this.grades ...
+   */
+  
+  double highestGrade() {
+    double highest = 0.0;
+    for (int i = 0; i < grades.length; i++) {
+      if (highest < grades[i]) {
+        highest = grades[i];
+      }
+    }
+    return highest;
+  } 
 }
